153ee9dcc0305071ece77e15cc55ea7a9e212dcf
Kulkarni <pooja-k.kulkarni@capgemini.com>
2019-12-12 09:41:13 +0530

    added application

---
+++ src/test/java/junitwebapp/JunitwebappTest.java
@@ -0,0 +1,25 @@
+package junitwebapp;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class JunitwebappTest {
+
+	@Before
+	public void setUp() throws Exception {
+		System.out.println("setting up");
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void test() {
+	
+	}
+
+}
